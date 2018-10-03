e375dd76da4e2a39adbeaba5c3e5de20aebd4a7b
RAYHANE BELAROUSSI <21709251@ad.ut-capitole.fr>
2018-10-03 10:08:54 +0200

    add: exit on close

--- src/tuto_git/Application.java
+++ src/tuto_git/Application.java
@@ -1,25 +1,33 @@
 package tuto_git;
 
 import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class Application extends Frame {
 
-	public static void main(String[] args) {
-		new Application();
-	}
-	
-	public Application() {
-		super();
-		
-		Modèle modl = new Modèle();
-		Contrôleur ctrl = new Contrôleur(modl); 
-		Vue vue = new Vue();
-		modl.addObserver(vue);
-		vue.addActionListener(ctrl);
-		this.setTitle("tutoriel git");
-		this.add(vue);
-		this.pack();
-		this.setVisible(true);
-	}
+    public static void main(String[] args) {
+        new Application();
+    }
+
+    public Application() {
+        super();
+
+        Modèle modl = new Modèle();
+        Contrôleur ctrl = new Contrôleur(modl);
+        Vue vue = new Vue();
+        modl.addObserver(vue);
+        vue.addActionListener(ctrl);
+        this.setTitle("tutoriel git");
+        this.add(vue);
+        this.pack();
+        this.setVisible(true);
+        this.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                dispose();
+            }
+        }
+        );
+    }
 
 }
